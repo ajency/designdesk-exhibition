d5de4175e4c32da7e8cbe4e5b84e9f26ee7c8f48
Mamata <mamata@ajency.in>
2022-09-15 08:55:39 +0530

    services cards

--- wp-content/themes/designdesk-child/assets/js/theme-scripts.js
+++ wp-content/themes/designdesk-child/assets/js/theme-scripts.js
@@ -1,46 +1,90 @@
 //number counter animation
-$('.stat-number mark').each(function () {
-    $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-    }, {
+$(".stat-number mark").each(function () {
+  $(this)
+    .prop("Counter", 0)
+    .animate(
+      {
+        Counter: $(this).text(),
+      },
+      {
         duration: 4000,
-        easing: 'swing',
+        easing: "swing",
         step: function (now) {
-            // $(this).text(Math.ceil(now));
-           $(this).text(Math.ceil(now).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
-        }
-    });
+          // $(this).text(Math.ceil(now));
+          $(this).text(
+            Math.ceil(now)
+              .toString()
+              .replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")
+          );
+        },
+      }
+    );
 });
 
 //logo carousel
-$('.logo-carousel .gb-container-content').slick({
-    speed: 5000,
-    autoplay: true,
-    autoplaySpeed: 0,
-    centerMode: true,
-    cssEase: 'linear',
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    infinite: true,
-    initialSlide: 1,
-    arrows: false,
-    buttons: false,
-    pauseOnHover:true,
-    accesibility: false,
-    draggable: false,
-    swipe: false,
-    touchMove: false,
-    responsive: [
-        {
-            breakpoint: 769,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                speed: 5000,
-                autoplaySpeed: 0,
-                autoplay: true,
-            }
-        }
-    ]
+$(".logo-carousel .gb-container-content").slick({
+  speed: 5000,
+  autoplay: true,
+  autoplaySpeed: 0,
+  centerMode: true,
+  cssEase: "linear",
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  variableWidth: true,
+  infinite: true,
+  initialSlide: 1,
+  arrows: false,
+  buttons: false,
+  pauseOnHover: true,
+  accesibility: false,
+  draggable: false,
+  swipe: false,
+  touchMove: false,
+  responsive: [
+    {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 5000,
+        autoplaySpeed: 0,
+        autoplay: true,
+      },
+    },
+  ],
+});
+
+//service cards
+$(document).ready(function () {
+  
+    $(window).on("resize", function (e) {
+    checkScreenSize();
+  });
+
+  checkScreenSize();
+
+  function checkScreenSize() {
+    var newWindowWidth = $(window).width();
+    if (newWindowWidth < 992) {
+      checkCardTitle(40);
+      $(".service-card").click(function () {
+        $(this).find(".service-card-details").toggleClass("show");
+      });
+    } else {
+      checkCardTitle(60);
+      $(".service-card").hover(function () {
+        $(this).find(".service-card-details").toggleClass("show");
+      });
+    }
+  }
+  function checkCardTitle($addPadding){
+    $cartTitleHeight = $('.service-card');
+
+    $cartTitleHeight.each(function() {
+        $titleHeight = $(this).find('.service-card-title').outerHeight();
+        $finalTitleHeight = $titleHeight+$addPadding;
+        $(this).find('.service-card-details').css('transform','translateY(calc(100% - '+$finalTitleHeight+'px))');
+        console.log($titleHeight);
+      });
+  }
 });
--- wp-content/themes/designdesk-child/shortcodes/shortcodes.php
+++ wp-content/themes/designdesk-child/shortcodes/shortcodes.php
@@ -43,6 +43,9 @@ function link_with_icon($atts)
         'link-title'    => 'Link Title',
     );
     $a = shortcode_atts($default, $atts);
-    return '<span class="link-with-icon"><a href="' . $a['link'] . '">' . $a['link-title'] . '</a><span class="icon"><svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.33594 10.667L6.0026 6.00033L1.33594 1.33366" stroke="#2471B5" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/></svg></span>';
+    return '<span class="link-with-icon"><a href="' . $a['link'] . '">' . $a['link-title'] . '</a><span class="icon"><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M5.33594 12.667L10.0026 8.00033L5.33594 3.33366" stroke="#2471B5" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    </span>';
 }
 add_shortcode('dd_link_with_icon', 'link_with_icon');
