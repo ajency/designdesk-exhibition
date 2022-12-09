ab574867de91f024c172d757069c1e37313cd199
Mamata <mamata@ajency.in>
2022-12-09 20:32:47 +0530

    Portfolio LP - Filters ( looped through portfolios & created a list)

--- wp-content/themes/designdesk-child/page-templates/portfolio.php
+++ wp-content/themes/designdesk-child/page-templates/portfolio.php
@@ -24,17 +24,72 @@
                         <hr class="hide-md-up wp-block-separator alignfull has-alpha-channel-opacity is-style-wide">
 
                         <!-- Filters -->
+                        <?php 
+                                $portfoliosQuery = new WP_Query([
+                                    'post_type' => 'dd_portfolio',
+                                    'posts_per_page' => -1
+                                ]);
+                                $stallSizes = [];
+                                $locations = [];
+                                $industries = [];
+
+                                function cleanStr($string){
+                                    // Replaces all spaces with hyphens.
+                                    $string = str_replace(' ', '-', $string);
+                                
+                                    // Removes special chars.
+                                    $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string);
+                                    // Replaces multiple hyphens with single one.
+                                    $string = preg_replace('/-+/', '-', $string);
+                                    
+                                    return strtolower($string);
+                                }
+                        ?>
+                        <?php if($portfoliosQuery->have_posts()): ?>
+
+                            <?php
+                                while($portfoliosQuery->have_posts()) : $portfoliosQuery->the_post();
+                                    $stallSize = get_field('stall_size');
+                                    if(!in_array($stallSize, $stallSizes, true) AND !empty($stallSize)):
+                                        array_push($stallSizes, $stallSize);
+                                    endif;
+                                    $location = get_field('location');
+                                    if(!in_array($location, $locations, true) AND !empty($location)):
+                                        array_push($locations, $location);
+                                    endif;
+
+                                    $term_list = wp_get_post_terms( get_the_ID() , 'dd_industries', array( 'fields' => 'names' ) );
+
+                                    $industry_list = $term_list;
+                                    if(!in_array($industry_list, $industries, true) AND !empty($industry_list)):
+                                        array_push($industries, $industry_list);
+                                    endif;
+
+                                endwhile;
+
+                                $combinedIndustries = [];
+                                foreach( $industries as $allindustry ): 
+                                        foreach( $allindustry as $singleIndustry ): 
+                                            if(!in_array($singleIndustry, $combinedIndustries, true) AND !empty($singleIndustry)):
+                                                array_push($combinedIndustries, $singleIndustry);
+                                            endif;
+                                            
+                                         endforeach; 
+                                endforeach;
+                                ?>
+
                         <div class="dd-filters">
                             <div class="dd-filters__wraper">
-
                             <div class="field-group">
                                 <div id="reset-button" onclick="resetFilter(this)"></div>
                                 <label for="stallSize">Stall Size</label>
                                 <select class="dd-select" id="stallSizes">
                                     <option value="hide">Select Stall Size</option>
-                                    <option value="1">100 sqm</option>
-                                    <option value="2">120 sqm</option>
-                                    <option value="3">200 sqm</option>
+                                    <?php
+                                        foreach( $stallSizes as $stallSize ) {?>
+                                            <option value="<?php echo $stallSize ?>"><?php echo $stallSize ?> sqm</option>
+                                        <?php }
+                                    ?>
                                 </select> 
                             </div>
                             <div class="field-group">
@@ -42,9 +97,11 @@
                                 <div id="reset-button" onclick="resetFilter(this)"></div>
                                 <select class="dd-select" id="industries">
                                     <option value="hide">Select Industry</option>
-                                    <option value="4">Pharmaceutical</option>
-                                    <option value="5">Agriculture</option>
-                                    <option value="6">Media and Telecommunication</option>
+                                    <?php
+                                        foreach( $combinedIndustries as $industry_name ) {?>
+                                            <option value="<?php echo cleanStr($industry_name) ?>"><?php echo $industry_name ?></option>
+                                        <?php }
+                                    ?>
                                 </select> 
                             </div>
                             <div class="field-group">
@@ -52,14 +109,18 @@
                                 <div id="reset-button" onclick="resetFilter(this)"></div>
                                 <select class="dd-select" id="locations">
                                     <option value="hide">Select Location</option>
-                                    <option value="8">New Delhi</option>
-                                    <option value="9">Germany</option>
-                                    <option value="10">Bengaluru</option>
+                                    <?php
+                                        foreach( $locations as $location ) {?>
+                                            <option value="<?php echo cleanStr($location) ?>"><?php echo $location ?></option>
+                                        <?php }
+                                    ?>
                                 </select> 
                             </div>
 
                             </div>
                         </div>
+
+                        <?php  endif; ?>
 
                         <!-- Portfolios -->
                         <?php 
