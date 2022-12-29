9f8729ecf2be8f25da0e683d33c33ac55dfd7918
Mamata <mamata@ajency.in>
2022-12-29 18:47:06 +0530

    Issue Fixes

--- wp-content/themes/designdesk-child/template-parts/content-banner.php
+++ wp-content/themes/designdesk-child/template-parts/content-banner.php
@@ -20,7 +20,7 @@
         <div class="wp-block-genesis-blocks-gb-columns hero-banner mt-0 gb-layout-columns-1 one-column gb-background-cover gb-background-no-repeat alignfull" <?php if($banner_image_mob_array): ?> data-mobBg="<?php echo $banner_image_mob_array['url']; ?>"<?php endif; ?> style="background-image:url(<?php if($banner_image_array): echo $banner_image_array['url']; else: echo site_url() . '/wp-content/themes/designdesk-child/assets/images/placeholder.jpg'; endif; ?>)">
             <div class="gb-layout-column-wrap gb-block-layout-column-gap-2 gb-is-responsive-column">
                 <div class="wp-block-genesis-blocks-gb-column alignwide gb-block-layout-column gb-is-vertically-aligned-center">
-                    <div class="gb-block-layout-column-inner">
+                    <div class="gb-block-layout-column-inner <?php if ($postName == 'contact-us'): echo "contact-us"; endif; ?>">
                         <p class="h1-semi-600 banner-heading has-white-color has-text-color"><?php if($banner_heading): echo $banner_heading; else: echo single_term_title(); endif; ?></p>
                         <?php if($banner_text): ?><p class="h4-reg-400 banner-text has-white-color has-text-color"><?php echo $banner_text ?></p><?php endif; ?>
                         <hr class="wp-block-separator has-alpha-channel-opacity separator-horizontal-orange">
