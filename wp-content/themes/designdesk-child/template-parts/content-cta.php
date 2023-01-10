00f52b8399f0e276d06b02643b89e6b57276e1bc
Mamata <mamata@ajency.in>
2023-01-10 10:34:37 +0530

    Contact-CTA template changes + removed unwanted plugins & pages

--- wp-content/themes/designdesk-child/template-parts/content-cta.php
+++ wp-content/themes/designdesk-child/template-parts/content-cta.php
@@ -1,8 +1,17 @@
 <?php
-    $cta_visibility = get_field('cta_visibility');
-    $cta_heading = get_field( 'cta_heading' );
-    $cta_image_array = get_field( 'cta_image' );
-    $cta_array = get_field( 'cta' );
+    $term = get_queried_object();
+    if(is_archive()){
+        $cta_visibility = get_field('cta_visibility', $term);
+        $cta_heading = get_field( 'cta_heading', $term );
+        $cta_image_array = get_field( 'cta_image', $term );
+        $cta_array = get_field( 'cta', $term );
+    }else{
+        $cta_visibility = get_field('cta_visibility');
+        $cta_heading = get_field( 'cta_heading' );
+        $cta_image_array = get_field( 'cta_image' );
+        $cta_array = get_field( 'cta' );
+    }
+
     if ($cta_visibility == 1 ) : ?>
         <div class="wp-block-genesis-blocks-gb-columns bg-image-cta belowFold gb-layout-columns-1 gb-1-col-equal alignfull in-viewport">
             <div class="gb-layout-column-wrap gb-block-layout-column-gap-2 gb-is-responsive-column">
